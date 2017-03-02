7cc4043ee4efd5eb0d2dd85c1fc890a9ee8fc1be
kerry <kerry@miglab.com>
2017-03-02 10:13:16 +0800

    +中间通讯层处理二进制包问题

--- public/comm/core_interface.h
+++ public/comm/core_interface.h
@@ -3,13 +3,14 @@
 
 #include <string>
 
-namespace net{
+namespace net {
 bool core_connect_net(const char *addr, unsigned short port);
 
 bool core_connect_ipc(const char *path);
 
-bool core_get(int socket, const char *key, size_t key_len,
-		std::string& value, int &flags, int &return_code);
+bool core_get(int socket, const char *key, size_t key_len, std::string& value,
+              int &flags, int &return_code);
+
 
 void core_close();
 
